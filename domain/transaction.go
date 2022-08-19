1122d13af2c4668dbb5359cb38d7f9ee2ea56248
Jemima Luz <contatojemima@gmail.com>
2022-08-19 15:16:05 -0300

    feat: domain initial implementation
    - create CreditCard struct with constructor
    - create Transaction struct with constructor
    - create validation function for transactions

---
+++ domain/transaction.go
@@ -0,0 +1,34 @@
+package domain
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+
+type Transaction struct {
+	ID           string
+	Amount       float64
+	Status       string
+	Description  string
+	Store        string
+	CreditCardId string
+	CreatedAt    time.Time
+}
+
+func NewTransaction() *Transaction {
+	transaction := &Transaction{}
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+	return transaction
+}
+
+func (t *Transaction) ProcessAndValidate(creditCard *CreditCard) {
+	if t.Amount+creditCard.Balance > creditCard.Limit {
+		t.Status = "rejected"
+	} else {
+		t.Status = "approved"
+		creditCard.Balance = creditCard.Balance + t.Amount
+	}
+}
