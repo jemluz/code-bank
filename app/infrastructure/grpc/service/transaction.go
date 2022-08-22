d41f5f5b110d44789645f1e2caed8feb9dcb4a35
Jemima Luz <contatojemima@gmail.com>
2022-08-21 22:45:15 -0300

    feat: setup grpc
    - create grpc folder
    - setup protocol buffers contract at grpc/payment.proto
    -- define data objects formats for PaymentRequest and CreditCard
    -- payment service implementation
    - create Makefile to compile .proto files and generate .pb.go files (where is the get and set functions of our app ecosystem)
    - run `go get google.golang.org/grpc` to force download of packages used by .pb.go files
    
    - create grpc service and server
    
    -- create grpc/service folder to implements transaction.go service
        --- implements payment function to fill DTO with grpc’s driven data, and process transaction with filled DTO using usecase
        --- payment function is same as a controller
    -- run `go get google.golang.org/protobuf` to get service/transaction.go packages
    -- create server grpc/server folder to implements app server (like an express)
        --- basically the serve calls the service, service needs a server
    
    - fix some syntax errors
    
    - setup kafka with grpc inside main.go
    
    -- setup kafka producer at main.go
    -- setup server and service at main.go
    -- run `go run main.go` at bash appbank terminal
    -- resolve `databasesql: unknown driver "postgres" (forgotten import?)` output erro with adding `_ "github.com/lib/pq"` inside imports

---
+++ app/infrastructure/grpc/service/transaction.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"context"
+
+	"github.com/codeedu/codebank/dto"
+	"github.com/codeedu/codebank/infrastructure/grpc/pb"
+	"github.com/codeedu/codebank/usecase"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type TransactionService struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+	pb.UnimplementedPaymentServiceServer
+}
+
+func NewTransactionService() *TransactionService {
+	return &TransactionService{}
+}
+
+func (t *TransactionService) Payment(ctx context.Context, in *pb.PaymenRequest) (*empty.Empty, error) {
+	transactionDTO := dto.Transaction{
+		Name:            in.GetCreditCard().Name,
+		Number:          in.CreditCard.GetNumber(),
+		ExpirationMonth: in.GetCreditCard().GetExpirationMonth(),
+		ExpirationYear:  in.GetCreditCard().GetExpirationYear(),
+		CVV:             in.GetCreditCard().GetCvv(),
+		Amount:          in.GetAmount(),
+		Store:           in.GetStore(),
+		Description:     in.GetDescription(),
+	}
+
+	transaction, err := t.ProcessTransactionUseCase.ProcessTransaction(transactionDTO)
+
+	if err != nil {
+		return &empty.Empty{}, status.Error(codes.FailedPrecondition, err.Error())
+	}
+
+	if transaction.Status != "approved" {
+		return &empty.Empty{}, status.Error(codes.FailedPrecondition, "transaction rejected by the bank")
+	}
+
+	return &empty.Empty{}, nil
+}
--- app/main.go
+++ app/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/codeedu/codebank/domain"
+	_ "github.com/lib/pq"
+
+	"github.com/codeedu/codebank/infrastructure/grpc/server"
+	"github.com/codeedu/codebank/infrastructure/kafka"
 	"github.com/codeedu/codebank/infrastructure/repository"
 	"github.com/codeedu/codebank/usecase"
 )
@@ -14,23 +17,34 @@ func main() {
 	db := setupDB()
 	defer db.Close()
 
-	cc := domain.NewCreditCard()
-	cc.Number = "1234"
-	cc.Name = "Jemima"
-	cc.ExpirationMonth = 7
-	cc.ExpirationYear = 2021
-	cc.CVV = 123
-	cc.Limit = 1000
-	cc.Balance = 0
+	// cc := domain.NewCreditCard()
+	// cc.Number = "1234"
+	// cc.Name = "Jemima"
+	// cc.ExpirationMonth = 7
+	// cc.ExpirationYear = 2021
+	// cc.CVV = 123
+	// cc.Limit = 1000
+	// cc.Balance = 0
 
-	repo := repository.NewTransactionRepositoryDB(db)
-	repo.CreateCreditCard(*cc)
+	// repo := repository.NewTransactionRepositoryDB(db)
+	// repo.CreateCreditCard(*cc)
+
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	serveGRPC(processTransactionUseCase)
 }
 
-func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecase.UseCaseTransaction {
 	transactionRepository := repository.NewTransactionRepositoryDB(db)
 	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
 	return useCase
+}
+
+func setupKafkaProducer() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer("host.docker.internal:9094")
+	return producer
 }
 
 func setupDB() *sql.DB {
@@ -45,8 +59,15 @@ func setupDB() *sql.DB {
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal("error connection to database")
+		log.Fatal("error connection to database", err)
 	}
 
 	return db
+}
+
+func serveGRPC(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	fmt.Println("Rodando grpc server...")
+	grpcServer.Serve()
 }
